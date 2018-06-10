30f37ba610326af505ba10eae9b2a6436a2bdf20
Nestor Segura <nestor@Nestors-Air.fritz.box>
2018-06-10 02:19:46 +0200

    add function binary to roman

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -30,7 +30,8 @@ class App extends Component {
             errorMessage: null
         },
         romanNumbers: ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"],
-        decimalNumbers: [1000,900,500,400,100,90,50,40,10,9,5,4,1],
+        decimalNumbers: [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+        binaryNumbers: [512, 256, 128, 64, 32, 16, 8, 4, 2, 1],
         loopIndex: 0,
         opValue: 0,
         conversionValue: "",
@@ -38,8 +39,8 @@ class App extends Component {
     }
 
 
-    inputChangeHandler = (e) => {        
-        if(this.state.operation) {
+    inputChangeHandler = (e) => {
+        if (this.state.operation) {
             this.setState({
                 ...this.state.numberConverter,
                 numberConverter: {
@@ -47,15 +48,15 @@ class App extends Component {
                     value: e.target.valueAsNumber
                 }
             })
-    
-            if(this.state.opValue !== 0) {
-                this.setState({opValue: this.state.numberConverter.value})
+
+            if (this.state.opValue !== 0) {
+                this.setState({ opValue: this.state.numberConverter.value })
             }
-        }else {
+        } else {
             alert("please select an option to start the conversion")
         }
-        
-        
+
+
     }
 
     rbChangeHandler = (e) => {
@@ -94,47 +95,87 @@ class App extends Component {
     }
 
     setConversionOperation = (radioButtonValue) => {
-        switch(radioButtonValue) {
-            case "d2r": 
-               return this.decimalsToRoman;
+        switch (radioButtonValue) {
+            case "d2r":
+                return this.decimalsToRoman;
+                break;
+            case "b2r":
+                return this.binaryToRoman;
         }
     }
 
     decimalsToRoman = (value) => {
-            // not working 
-            if( value < 1) {
-                alert("this program only accepts numbers grater than 0")
-                this.setState({
-                    ...this.state.numberConverter,
-                    numberConverter: {
-                        value: 0
-                    },
-                    conversionValue: ""
-                })
-            }else if(value > 3999) {
-                alert("this program only accepts numbers until 3999")
-                this.setState({
-                    ...this.state.numberConverter,
-                    numberConverter: {
-                        value: 0
-                    },
-                    conversionValue: ""
-                })
-            }
-            let i = 0;
-            let romanNumber =""; 
-            let localValue = value; 
-            while(localValue > 0) {
-                if(localValue - this.state.decimalNumbers[i] >= 0) {
-                    romanNumber = romanNumber + this.state.romanNumbers[i]
-                    localValue = localValue - this.state.decimalNumbers[i]
-                }else {
-                   i = i+1
-                }
-            }
-
-            return romanNumber;
+        // not working 
+        if (value < 1) {
+            alert("this program only accepts numbers grater than 0")
+            this.setState({
+                ...this.state.numberConverter,
+                numberConverter: {
+                    value: 0
+                },
+                conversionValue: ""
+            })
+        } else if (value > 3999) {
+            alert("this program only accepts numbers until 3999")
+            this.setState({
+                ...this.state.numberConverter,
+                numberConverter: {
+                    value: 0
+                },
+                conversionValue: ""
+            })
         }
+        let i = 0;
+        let romanNumber = "";
+        let localValue = value;
+        while (localValue > 0) {
+            if (localValue - this.state.decimalNumbers[i] >= 0) {
+                romanNumber = romanNumber + this.state.romanNumbers[i]
+                localValue = localValue - this.state.decimalNumbers[i]
+            } else {
+                i = i + 1
+            }
+        }
+
+        return romanNumber;
+    }
+
+    binaryToRoman = (value) => {
+        let decimalNumber = 0;
+        
+        if(value) {
+            if(value.toString().length < 10){
+                let binaryToDecimal = value.toString().split("").map((n, i) => {
+                    if(n === "1" || n === "0") {
+                        if (n === "1") {
+                            decimalNumber += this.state.binaryNumbers[i];
+                        }  
+                    }else {
+                        alert("please enter a valid binary numer for example 101");
+                        this.setState({
+                            ...this.state.numberConverter,
+                            numberConverter: {
+                                value: ""
+                            }
+                        })
+                    }
+                })
+            }else {
+                alert("this function only accepts binary numbers until 10 digits")
+
+                this.setState({
+                    ...this.state.numberConverter,
+                    numberConverter: {
+                        value: ""
+                    }
+                })
+            }
+        }
+
+        if(decimalNumber > 0) {
+            return this.decimalsToRoman(decimalNumber)
+        }
+    }
 
     render() {
 
@@ -165,8 +206,8 @@ class App extends Component {
                     errorToggle={this.state.error.show}
                     errorMessage={this.state.errorMessage}
                     onKeyPressed={e => this.errorMessageHandler(e)}
-                    decimalToRomanHandler = {this.state.operation}
-                    conversionValue = {this.state.conversionValue}
+                    decimalToRomanHandler={this.state.operation}
+                    conversionValue={this.state.conversionValue}
                 />
 
                 <span>{this.state.error.show ? this.state.error.errorMessage : null}</span>
